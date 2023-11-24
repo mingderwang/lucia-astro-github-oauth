de1e39f14ced4a581487aade49752799e26a8b56
Ming-der Wang <mingderwang@gmail.com>
2023-11-24 17:49:39 +0800

    release to vercel fail because of cannot run sqlite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
-
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
+//import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: node({
-		mode: "standalone"
-	})
+  adapter: vercel(),
+  output: 'server',
+//	output: "server",
+//	adapter: node({
+//		mode: "standalone"
+//	})
 });
